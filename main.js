6310c81c359637d4e3f2903ffbf7637365b327ef
NguyenDatGH <tandatbtqb@gmail.com>
2024-09-20 00:51:26 +0700

    fix some err

--- main.js
+++ main.js
@@ -3,34 +3,36 @@ buttonHTML = document.getElementById("app__button");
 taskListHTML = document.getElementById("task__list");
 numberOfTasksHTML = document.getElementById("numberOfTasks");
 
-// console.log(inputHTML);
-// console.log(buttonHTML);
-// console.log(taskListHTML);
-
 let numberofTasks = 0;
-  buttonHTML.addEventListener("click", () => { 
-  numberofTasks++;
-  numberOfTasksHTML.textContent = numberofTasks;
-  console.log(numberOfTasks);
-  console.log(inputHTML.value);
-  const taskItem = document.createElement("li");
-  taskItem.innerHTML = `
-    <span>${numberofTasks}</span>
-    <p>${inputHTML.value}</p>
-    <button onClick="deleteTask(this)">DELETE</button>
-    `;
+buttonHTML.addEventListener("click", () => {
+  const content = inputHTML.value.trim();
+
+  if (content !== "") {
+    numberofTasks++;
+    numberOfTasksHTML.textContent = numberofTasks;
+    console.log(numberOfTasks);
+    console.log(inputHTML.value);
+    const taskItem = document.createElement("li");
+    taskItem.innerHTML = `
+      <span>${numberofTasks}</span>
+      <p>${inputHTML.value}</p>
+      <button onClick="deleteTask(this)">DELETE</button>
+      `;
     taskItem.classList.add("task__items");
     taskListHTML.appendChild(taskItem);
     console.log(taskItem);
     inputHTML.value = "";
+  }else{
+    console.log("empty input!");
+  }
 });
 
-  const deleteTask = (button) => {
+const deleteTask = (button) => {
   console.log(button);
   const taskItem = button.closest(".task__items");
   console.log(taskItem);
   taskItem.remove();
   numberofTasks--;
   numberOfTasksHTML.textContent = numberofTasks;
-  // console.log("click delete");
- }
+
+};
